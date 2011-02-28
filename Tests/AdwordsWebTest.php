daed35f64212e35bde4d660d4720b12050be28b4
tirnanog06 <matthewfitz@gmail.com>
2011-02-28 17:55:05 -0500

    Add Adwords Web Test

---
+++ Tests/AdwordsWebTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AntiMattr\GoogleBundle\Tests;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AntiMattr\GoogleBundle\Adwords;
+use AntiMattr\GoogleBundle\Adwords\Conversion;
+
+class AdwordsWebTest extends WebTestCase
+{
+    private $adwords;
+    private $client;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->client = $this->createClient();
+        $this->adwords = $this->kernel->getContainer()->get('google.adwords');
+    }
+
+    protected function tearDown()
+    {
+        $this->adwords = null;
+        $this->client = null;
+        parent::tearDown();
+    }
+
+    public function testConstructor()
+    {
+        $this->assertFalse($this->adwords->hasActiveConversion());
+        $this->assertNull($this->adwords->getActiveConversion());
+    }
+
+    public function testActivateGetConversion()
+    {
+        $this->adwords->activateConversionByKey('incorrect_conversion');
+        $this->assertFalse($this->adwords->hasActiveConversion());
+
+        $this->adwords->activateConversionByKey('account_create');
+        $this->assertTrue($this->adwords->hasActiveConversion());
+
+        $this->assertNotNull($this->adwords->getActiveConversion());
+        $this->assertNull($this->adwords->getActiveConversion());
+    }
+}
