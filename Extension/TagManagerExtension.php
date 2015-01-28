fc7d473c31e5a4776654250ef75590ce8e201454
Andrey Kucherenko <andku@ciklum.com>
2015-01-28 18:53:03 +0100

    Configurable container id for Tag manager

---
+++ Extension/TagManagerExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AntiMattr\GoogleBundle\Extension;
+
+use AntiMattr\GoogleBundle\Helper\TagManagerHelper;
+
+/**
+ * Tag Manager Extension
+ */
+class TagManagerExtension extends \Twig_Extension
+{
+    private $tagManagerHelper;
+
+    /**
+     * Constructor
+     *
+     * @param TagManagerHelper $tagManagerHelper
+     */
+    public function __construct(TagManagerHelper $tagManagerHelper)
+    {
+        $this->tagManagerHelper = $tagManagerHelper;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGlobals()
+    {
+        return [
+            'google_tag_manager' => $this->tagManagerHelper,
+        ];
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'google_tag_manager';
+    }
+}
