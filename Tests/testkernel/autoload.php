b459ea215af2cbd7fcc438b2d4eda010be3f99f5
Alex Vasilenko <oleksandr.vasylenko@meinfernbus.de>
2017-01-29 14:07:33 +0100

    enable travis fix tests to pass bump requirements version due to unmet phpunit dependency

--- Tests/testkernel/autoload.php
+++ Tests/testkernel/autoload.php
@@ -1,23 +1,4 @@
 <?php
 
-require_once $_SERVER['VENDOR_LIB'].'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony'                                           => $_SERVER['VENDOR_LIB'].'/symfony/src',
-    'AntiMattr'                                         => __DIR__.'/../../../../',
-    'Doctrine\\Common\\DataFixtures'                    => $_SERVER['VENDOR_LIB'].'/doctrine-data-fixtures/lib',
-    'Doctrine\\Common'                                  => $_SERVER['VENDOR_LIB'].'/doctrine-common/lib',
-    'Doctrine\\DBAL\\Migrations'                        => $_SERVER['VENDOR_LIB'].'/doctrine-migrations/lib',
-    'Doctrine\\DBAL'                                    => $_SERVER['VENDOR_LIB'].'/doctrine-dbal/lib',
-    'Doctrine\\ORM'                                     => $_SERVER['VENDOR_LIB'].'/doctrine/lib',
-    'Zend'                                              => $_SERVER['VENDOR_LIB'].'/zend/library'
-));
-$loader->registerPrefixes(array(
-    'Swift_'     => $_SERVER['VENDOR_LIB'].'/swiftmailer/lib/classes',
-    'Twig_'      => $_SERVER['VENDOR_LIB'].'/twig/lib'
-));
-$loader->registerNamespaceFallbacks(array($_SERVER['VENDOR_LIB'].'/bundles'));
-$loader->register();
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/TestKernel.php';
